4d822231fdd218caab76ac7939785d03fbdf173c
bawanj98 <bawanj98@gmail.com>
2021-12-07 15:47:03 +0300

    adding the variables as string and some function

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,3 +1,24 @@
 //! Define the model class Note here
 
-class Note {}
+
+class Note {
+  String thetitle;
+  String thecontent;
+
+  Note(String tit, String con) {
+    thetitle = tit;
+    thecontent = con;
+  }
+  String getTitle() {
+    return thetitle;
+  }
+  String getContent() {
+    return thecontent;
+  }
+  void setTitle(String t) {
+    thetitle = t;
+  }
+  void setContent(String c) {
+    thetitle = c;
+  }
+}
